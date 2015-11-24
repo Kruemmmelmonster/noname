b75c82c36faa5a5b3009e8fd7577c5b61b8a774f
Børge <boerge@grunicke.de>
2015-11-24 10:40:08 +0100

    c

---
+++ Kruemmel_monsters/Kruemmel_monsters/build/Model/Checkout.cs
@@ -0,0 +1,24 @@
+﻿using noname;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kruemmel_monsters.build.Model
+{
+    class Checkout
+    {
+        private List<Product> products = new List<Product>();
+
+        public void addProduct (Product product) {
+            products.Add(product);
+        }
+
+        public void removeProduct (Product product) {
+            products.Remove(product);
+        }
+
+
+    }
+}
