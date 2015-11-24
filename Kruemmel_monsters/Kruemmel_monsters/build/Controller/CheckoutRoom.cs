77f1aede692a76d00df28b8e54597d3c68e07972
Børge <boerge@grunicke.de>
2015-11-24 11:11:49 +0100

    sdwer

---
+++ Kruemmel_monsters/Kruemmel_monsters/build/Controller/CheckoutRoom.cs
@@ -0,0 +1,26 @@
+﻿using Kruemmel_monsters.build.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kruemmel_monsters.build.Controller
+{
+    class CheckoutRoom
+    {
+        private List<Checkout> checkouts;
+
+        public CheckoutRoom() { }
+
+        public void openCheckout()
+        {
+            checkouts.Add(new Checkout());
+        }
+
+        public void closeCheckout(int id)
+        {
+            checkouts.RemoveAt(id);
+        }
+    }
+}
